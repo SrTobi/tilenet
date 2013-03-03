f182b410e73d58254ef1b1ce795c490e2915467d
SirTobi <Code.DataByte@gmail.com>
2013-03-03 13:38:18 +0100

    Declaration extended.

--- include/tilenet.h
+++ include/tilenet.h
@@ -24,24 +24,36 @@ extern "C" {
 #	endif
 #endif
 
-#define TNERRI_DESCRIPTION	1
+#define TNERRI_DESCRIPTION	0
 
 typedef unsigned int TNERROR;
 typedef unsigned int TNERRINFO;
 typedef unsigned int TNIDTYPE;
 
+typedef TNIDTYPE TNWEAKID;
 typedef TNIDTYPE TNSERVERID;
 
-TNAPI TNERROR tilenet_convert_to_wstring(const char* src, wchar_t* dest);
-TNAPI TNERROR tilenet_convert_to_astring(const wchar_t* src, char* dest);
+TNAPI TNERROR tilenet_convert_to_wstring(const char* src, wchar_t* dest, size_t size);
+TNAPI TNERROR tilenet_convert_to_astring(const wchar_t* src, char* dest, size_t size);
 
+/**** error management ****/
 TNAPI TNERROR tilenet_get_error_string(TNERROR err, TNERRINFO infono, wchar_t* dest, size_t buflen);
 TNAPI TNERROR tilenet_get_error_int(TNERROR err, TNERRINFO infono, int* dest);
-TNAPI TNERROR tilenet_get_info_list(TNERROR err, TNERRINFO* dest, size_t buflen, size_t* cpylen);
+TNAPI TNERROR tilenet_get_info_list(TNERROR err, TNERRINFO* dest, size_t buflen, size_t* copyed);
 
+/**** id management *****/
+TNAPI TNERROR tilenet_addref(TNIDTYPE id);
+TNAPI TNERROR tilenet_release(TNIDTYPE id);
+TNAPI TNERROR tilenet_weak(TNIDTYPE id, TNIDTYPE* weak);
+TNAPI TNERROR tilenet_unweak(TNIDTYPE weak, TNIDTYPE* id);
+TNAPI TNERROR tilenet_refcount(TNIDTYPE id, size_t* refs);
+TNAPI TNERROR tilenet_weakcount(TNIDTYPE id, size_t* refs);
+TNAPI TNERROR tilenet_gc(TNSERVERID id, size_t* collected);
+
+/**** server management ****/
 TNAPI TNERROR tilenet_create_server(TNSERVERID* newid);
 TNAPI TNERROR tilenet_destroy_server(TNSERVERID srvid);
-
+TNAPI TNERROR tilenet_get_server_list(TNSERVERID* dest, size_t buflen, size_t* copyed);
 
 
 
