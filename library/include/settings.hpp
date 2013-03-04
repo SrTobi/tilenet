d447e02ecd60a489320a30584e2cace3a44ac396
SirTobi <Code.DataByte@gmail.com>
2013-03-04 01:45:28 +0100

    Settings added.

---
+++ library/include/settings.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _SETTINGS_HPP
+#define _SETTINGS_HPP
+
+
+#include <string>
+
+#include "tilenet.h"
+
+#define override
+
+typedef unsigned int IdType;
+typedef std::wstring string;
+
+
+#include "utils/tilenet_object.hpp"
+#include "utils/ptr.hpp"
+
+#endif
