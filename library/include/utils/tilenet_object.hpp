54869ef66b40e77adf5e217e3041ceeaa9daf9d8
SirTobi <Code.DataByte@gmail.com>
2013-03-20 15:32:20 +0100

    IdObject added.

--- library/include/utils/tilenet_object.hpp
+++ library/include/utils/tilenet_object.hpp
@@ -4,6 +4,8 @@
 
 /// @cond DEV
 
+#include <boost/thread/shared_mutex.hpp>
+#include <boost/thread/shared_lock_guard.hpp>
 #include "settings.hpp"
 
 
@@ -22,13 +24,94 @@ public:
 	virtual ~TilenetObject();
 
 	virtual size_t destroy() = 0;
-	virtual TilenetObject* clone() = 0;
+	virtual shared_ptr<TilenetObject> clone() = 0;
 };
 
 
+template<typename T>
+class IdObject
+{
+	typedef TNID							id_type;
+	typedef std::vector<T>					list_type;
+	typedef std::queue<id_type>				queue_type;
+public:
+	IdObject();
+	virtual ~IdObject();
+
+	virtual override size_t destroy() = 0;
+	virtual override shared_ptr<TilenetObject> clone() = 0;
+
+	id_type id() const;
+
+	static id_type Register(const shared_ptr<T>& obj);
+	static shared_ptr<T> Resolve(id_type id);
+private:
+	id_type	mId;
+	static list_type			Objects;
+	static queue_type			FreeList;
+	static boost::shared_mutex	IdMutex;
+};
 
 
+template<typename T>
+IdObject<T>::IdObject()
+{
+}
 
+template<typename T>
+IdObject<T>::~IdObject()
+{
+}
+
+template<typename T>
+typename IdObject<T>::id_type IdObject<T>::id() const
+{
+	return mId;
+}
+
+template<typename T>
+typename IdObject<T>::id_type IdObject<T>::Register(const shared_ptr<T>& obj)
+{
+	boost::lock_guard<boost::shared_mutex> lock(IdMutex);
+
+	id_type newId;
+	if(FreeList.size())
+	{
+		newId = FreeList.front();
+		FreeList.pop();
+
+		assert(newId < Objects.size());
+		assert(!Objects.at(newId));
+		Objects[newId] = obj;
+	}else{
+		newId = Objects.size();
+		Objects.push_back(obj);
+	}
+
+	// TODO: Add random tag
+	
+	obj->id = newId;
+
+	return newId;
+}
+
+template<typename T>
+shared_ptr<T> IdObject<T>::Resolve(id_type id)
+{
+	boost::shared_lock<boost::shared_mutex> lock(IdMutex);
+
+	id_type rawId = TNEXTRACTID_ID(id);
+
+	if(rawId < Objects.size())
+	{
+		shared_ptr<T> obj = Objects[rawId].lock();
+
+		if(obj && obj->id() == id)
+			return obj;
+	}
+	
+	// TODO: throw!
+}
 
 
 /// @endcond
