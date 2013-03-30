741b2176ffb1f1c64b3243052c7eaf8cc10bf283
SirTobi <Code.DataByte@gmail.com>
2013-03-30 19:13:58 +0100

    tileset added.

--- library/include/server/tileset.hpp
+++ library/include/server/tileset.hpp
@@ -2,12 +2,26 @@
 #ifndef _TILESET_HPP
 #define _TILESET_HPP
 
-
+#include "settings.hpp"
 
 
 namespace srv {
 
+class Tileset
+	: public TilenetObject
+	, public IdObject<Tileset>
+{
+public:
+	Tileset(const string& name, TNFLAG flags);
+	~Tileset();
 
+	TNID registerTile(const string& name);
+	TNID registerTile(const string& name, TNID id);
+
+	virtual override void destroy();
+	virtual override shared_ptr<TilenetObject> clone();
+	virtual override void flush();
+};
 
 
 
--- library/source/server/tileset.cpp
+++ library/source/server/tileset.cpp
@@ -3,8 +3,41 @@
 
 
 namespace srv {
+	
 
+Tileset::Tileset( const string& name, TNFLAG flags )
+{
+	NOT_IMPLEMENTED();
+}
 
+Tileset::~Tileset()
+{
+	NOT_IMPLEMENTED();
+}
 
+TNID Tileset::registerTile( const string& name )
+{
+	NOT_IMPLEMENTED();
+}
+
+TNID Tileset::registerTile( const string& name, TNID id )
+{
+	NOT_IMPLEMENTED();
+}
+
+void Tileset::destroy()
+{
+	NOT_IMPLEMENTED();
+}
+
+shared_ptr<TilenetObject> Tileset::clone()
+{
+	NOT_IMPLEMENTED();
+}
+
+void Tileset::flush()
+{
+	NOT_IMPLEMENTED();
+}
 
 }
