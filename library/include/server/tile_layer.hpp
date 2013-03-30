04ca2935ef12f2c0d7ebf419964ebc38d8184791
SirTobi <Code.DataByte@gmail.com>
2013-03-30 18:07:22 +0100

    TileLayer added.

--- library/include/server/tile_layer.hpp
+++ library/include/server/tile_layer.hpp
@@ -2,12 +2,40 @@
 #ifndef _TILELAYER_HPP
 #define _TILELAYER_HPP
 
+#include "settings.hpp"
+#include "layer.hpp"
 
+
+namespace net {
+	class Message;
+}
 
 
 namespace srv {
 
+class TileLayer
+	: public Layer
+{
+public:
+	TileLayer(const Rect& size, const Ratio& ratio, TNFLAG flags);
+	~TileLayer();
 
+	void putTile(const Point& pos, TNTILE* tile);
+
+	const Ratio& ratio() const;
+	const Rect&	size() const;
+
+	override void destroy();
+	override shared_ptr<TilenetObject> clone();
+	override void flush();
+	override shared_ptr<net::Message> getStateMessage();
+
+
+
+
+
+private:
+};
 
 
 
--- library/source/server/tile_layer.cpp
+++ library/source/server/tile_layer.cpp
@@ -5,6 +5,50 @@
 namespace srv {
 
 
+TileLayer::TileLayer(const Rect& size, const Ratio& ratio, TNFLAG flags)
+{
+	NOT_IMPLEMENTED();
+}
+
+TileLayer::~TileLayer()
+{
+	NOT_IMPLEMENTED();
+}
+
+void TileLayer::putTile(const Point& pos, TNTILE* tile)
+{
+	NOT_IMPLEMENTED();
+}
+
+const Ratio& TileLayer::ratio() const
+{
+	NOT_IMPLEMENTED();
+}
+
+const Rect& TileLayer::size() const
+{
+	NOT_IMPLEMENTED();
+}
+
+override void TileLayer::destroy()
+{
+	NOT_IMPLEMENTED();
+}
+
+override shared_ptr<TilenetObject> TileLayer::clone()
+{
+	NOT_IMPLEMENTED();
+}
+
+override void TileLayer::flush()
+{
+	NOT_IMPLEMENTED();
+}
+
+shared_ptr<net::Message> TileLayer::getStateMessage()
+{
+	NOT_IMPLEMENTED();
+}
 
 
 }
