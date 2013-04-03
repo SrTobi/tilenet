6397af95cee2260c5dde874c05577d99bae3ce20
SirTobi <Code.DataByte@gmail.com>
2013-04-04 00:11:42 +0200

    Commandset added.

--- library/include/server/commandset.hpp
+++ library/include/server/commandset.hpp
@@ -2,12 +2,31 @@
 #ifndef _COMMANDSET_HPP
 #define _COMMANDSET_HPP
 
-
+#include "settings.hpp"
 
 
 namespace srv {
 
 
+class CommandSet
+	:	public TilenetObject
+	,	public IdObject<CommandSet>
+{
+public:
+	CommandSet(const string& name);
+	~CommandSet();
+
+	void inherit(const shared_ptr<CommandSet>& cs);
+
+	TNID registerCmd(const string& name, const string& defKey);
+	TNID registerCmd(const string& name, const string& defKey, TNID id);
+
+	virtual OVERRIDE void destroy();
+	virtual OVERRIDE shared_ptr<TilenetObject> clone();
+	virtual OVERRIDE void flush();
+
+private:
+};
 
 
 
--- library/source/server/commandset.cpp
+++ library/source/server/commandset.cpp
@@ -5,6 +5,47 @@
 namespace srv {
 
 
+CommandSet::CommandSet( const string& name )
+{
+	NOT_IMPLEMENTED();
+}
+
+CommandSet::~CommandSet()
+{
+	NOT_IMPLEMENTED();
+}
+
+void CommandSet::inherit( const shared_ptr<CommandSet>& cs )
+{
+	NOT_IMPLEMENTED();
+}
+
+TNID CommandSet::registerCmd( const string& name, const string& defKey )
+{
+	NOT_IMPLEMENTED();
+}
+
+TNID CommandSet::registerCmd( const string& name, const string& defKey, TNID id )
+{
+	NOT_IMPLEMENTED();
+}
+
+
+
+OVERRIDE void CommandSet::destroy()
+{
+	NOT_IMPLEMENTED();
+}
+
+OVERRIDE shared_ptr<TilenetObject> CommandSet::clone()
+{
+	NOT_IMPLEMENTED();
+}
+
+OVERRIDE void CommandSet::flush()
+{
+	NOT_IMPLEMENTED();
+}
 
 
 }
