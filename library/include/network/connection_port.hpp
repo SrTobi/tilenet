efdaae50c08a5bd4cfae04c7a5fe113524d3130e
SirTobi <Code.DataByte@gmail.com>
2013-04-05 17:20:22 +0200

    Disconnect logic to connection-port added.

--- library/include/network/connection_port.hpp
+++ library/include/network/connection_port.hpp
@@ -15,6 +15,7 @@ class Message;
 class ConnectionPort
 {
 public:
+	typedef std::function<void()> disconnect_handler_type;
 	typedef std::function<void(const std::shared_ptr<Message>&)> handler_type;
 	ConnectionPort();
 	~ConnectionPort();
@@ -22,13 +23,17 @@ public:
 	virtual void send(const std::shared_ptr<Message>& msg) = 0;
 	
 	void setHandler(const handler_type& func);
+	void setDisconnectHandler(const disconnect_handler_type& func);
 	bool hasHandler() const;
+	bool hasDisconnectHandler() const;
 protected:
-	void handleReceive(const std::shared_ptr<Message>& msg) const;
 	virtual void onHandlerSet();
+	void handleReceive(const std::shared_ptr<Message>& msg) const;
+	void handleDisconnect() const;
 
 private:
 	handler_type mHandler;
+	disconnect_handler_type mDisconnectHandler;
 };
 
 
--- library/source/network/connection_port.cpp
+++ library/source/network/connection_port.cpp
@@ -15,9 +15,16 @@ ConnectionPort::~ConnectionPort()
 {
 }
 
+
+void ConnectionPort::setDisconnectHandler( const disconnect_handler_type& func )
+{
+	mDisconnectHandler = func;
+}
+
 void ConnectionPort::setHandler( const handler_type& func )
 {
 	mHandler = func;
+	onHandlerSet();
 }
 
 void ConnectionPort::handleReceive( const std::shared_ptr<Message>& msg) const
@@ -36,7 +43,16 @@ bool ConnectionPort::hasHandler() const
 	return bool(mHandler);
 }
 
+bool ConnectionPort::hasDisconnectHandler() const
+{
+	return bool(mDisconnectHandler);
+}
 
+void ConnectionPort::handleDisconnect() const
+{
+	if(hasDisconnectHandler())
+		handleDisconnect();
+}
 
 
 }
