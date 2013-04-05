8e3a5bb8c0b785756b0419244a940107d8958ce1
SirTobi <Code.DataByte@gmail.com>
2013-04-05 17:20:35 +0200

    Local connection port added.

---
+++ library/include/network/local_connection_port.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef _LOCAL_CONNECTION_PORT_HPP
+#define _LOCAL_CONNECTION_PORT_HPP
+
+#include <boost/asio/io_service.hpp>
+
+#include "settings.hpp"
+#include "connection_port.hpp"
+
+
+namespace net {
+
+class Message;
+
+
+class LocalConnectionPort
+	: public ConnectionPort
+{
+public:
+	~LocalConnectionPort();
+
+	virtual OVERRIDE void send(const std::shared_ptr<Message>& msg);
+
+
+	static std::tuple<shared_ptr<LocalConnectionPort>, shared_ptr<LocalConnectionPort>> Create(boost::asio::io_service& s1, boost::asio::io_service& s2);
+private:
+	LocalConnectionPort(boost::asio::io_service& service);
+
+private:
+	handler_type mHandler;
+	boost::asio::io_service& mService;
+	weak_ptr<LocalConnectionPort> mConnectedPort;
+};
+
+
+}
+
+
+#endif
---
+++ library/source/network/local_connection_port.cpp
@@ -0,0 +1,41 @@
+#include "includes.hpp"
+
+#include "network/local_connection_port.hpp"
+
+
+namespace net {
+
+
+LocalConnectionPort::LocalConnectionPort( boost::asio::io_service& service )
+	: mService(service)
+{
+}
+
+LocalConnectionPort::~LocalConnectionPort()
+{
+	shared_ptr<LocalConnectionPort> other = mConnectedPort.lock();
+
+	if(other)
+		other->mService.post(std::bind(&LocalConnectionPort::handleDisconnect, other));
+}
+
+OVERRIDE void LocalConnectionPort::send( const std::shared_ptr<Message>& msg )
+{
+	shared_ptr<LocalConnectionPort> other = mConnectedPort.lock();
+
+	if(other)
+		other->mService.post(std::bind(&LocalConnectionPort::handleReceive, other, msg));
+}
+
+std::tuple<shared_ptr<LocalConnectionPort>, shared_ptr<LocalConnectionPort>> LocalConnectionPort::Create( boost::asio::io_service& s1, boost::asio::io_service& s2 )
+{
+	shared_ptr<LocalConnectionPort> p1(new LocalConnectionPort(s1));
+	shared_ptr<LocalConnectionPort> p2(new LocalConnectionPort(s2));
+
+	p1->mConnectedPort = p2;
+	p2->mConnectedPort = p1;
+
+	return std::make_tuple(p1, p2);
+}
+
+}
