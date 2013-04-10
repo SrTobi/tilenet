94f9267adeeeeae28d30a1783ba3ffa167c9216a
SirTobi <Code.DataByte@gmail.com>
2013-04-10 16:02:14 +0200

    protocol version to protocol.hpp added.

--- library/include/network/protocol.hpp
+++ library/include/network/protocol.hpp
@@ -7,12 +7,14 @@
 #include "message.hpp"
 
 
-#define PROTOCOL_MESSAGE(_name, _target)	namespace _target{ typedef MsgFormat<ids::_target::_name> _name; } template<> struct MsgFormat<ids::_target::_name>
-#define PROTOCOL_SERIALIZER(_ar)			friend class boost::serialization::access; template<class Archive> void serialize(Archive& _ar, const unsigned int version)
-
+#define PROTOCOL_MESSAGE(_name, _target)		namespace _target{ typedef MsgFormat<ids::_target::_name> _name; } template<> struct MsgFormat<ids::_target::_name>
+#define PROTOCOL_SERIALIZER(_ar)				friend class boost::serialization::access; template<class Archive> void serialize(Archive& _ar, const unsigned int version)
+#define PROTOCOL_MAKE_VERSION(_major, _minor)	uint32(uint32(_major) << 16 | uint32(_minor))
 
 
 namespace proto {
+
+static const uint32 protocol_version = PROTOCOL_MAKE_VERSION(1, 0);
 
 template<MsgId Id>
 struct MsgFormat
