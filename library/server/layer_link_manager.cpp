38c55381ca2dcd7ffc105917f31556d122c9d362
SirTobi <Code.DataByte@gmail.com>
2013-05-10 13:50:21 +0200

    LayerLinkManager added and mock implemented.

---
+++ library/server/layer_link_manager.cpp
@@ -0,0 +1,81 @@
+#include "includes.hpp"
+#include "layer_link_manager.hpp"
+
+
+namespace srv {
+
+
+
+LayerLinkManager::LayerLinkManager()
+{
+}
+
+LayerLinkManager::~LayerLinkManager()
+{
+}
+
+void LayerLinkManager::linkLayerToParticipant( const shared_ptr<Layer>& layer, const shared_ptr<Participant>& participant )
+{
+	unlinkParticipant(participant);
+	mLinks.emplace_back(layer, participant);
+}
+
+void LayerLinkManager::updateFrameLinks( const shared_ptr<Frame>& frame )
+{
+	NOT_IMPLEMENTED();
+}
+
+void LayerLinkManager::unlinkLayer( const shared_ptr<Layer>& layer )
+{
+	auto it = mLinks.begin();
+
+	while(it != mLinks.end())
+	{
+		if(it->first == layer)
+		{
+			mLinks.erase(it++);
+		}else{
+			it++;
+		}
+	}
+}
+
+void LayerLinkManager::unlinkParticipant( const shared_ptr<Participant>& participant )
+{
+	for(auto it = mLinks.begin(); it != mLinks.end(); ++it)
+	{
+		if(it->second == participant)
+		{
+			mLinks.erase(it);
+			return;
+		}
+	}
+}
+
+std::vector<shared_ptr<Participant>>&& LayerLinkManager::getLinkedParticipants(const shared_ptr<Layer>& layer) const
+{
+	std::vector<shared_ptr<Participant>> participants;
+
+	for(auto& p : mLinks)
+	{
+		if(p.first == layer)
+		{
+			participants.push_back(p.second);
+		}
+	}
+
+	return std::move(participants);
+}
+
+LayerLinkManager& LayerLinkManager::Inst()
+{
+	static LayerLinkManager inst;
+	return inst;
+}
+
+
+
+
+
+
+}
---
+++ library/server/layer_link_manager.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef _LAYER_LINK_MANAGER_HPP
+#define _LAYER_LINK_MANAGER_HPP
+
+
+#include <vector>
+#include "settings.hpp"
+
+
+
+namespace srv {
+
+class Layer;
+class Frame;
+class Participant;
+
+
+class LayerLinkManager
+{
+public:
+	LayerLinkManager();
+	~LayerLinkManager();
+
+	void linkLayerToParticipant(const shared_ptr<Layer>& layer, const shared_ptr<Participant>& participant);
+	void updateFrameLinks(const shared_ptr<Frame>& frame);
+	void unlinkLayer(const shared_ptr<Layer>& layer);
+	void unlinkParticipant(const shared_ptr<Participant>& participant);
+
+	std::vector<shared_ptr<Participant>>&& getLinkedParticipants(const shared_ptr<Layer>& layer) const;
+
+	static LayerLinkManager& Inst();
+private:
+	// MOCK!!!
+	std::vector<std::pair<shared_ptr<Layer>, shared_ptr<Participant>>> mLinks;
+};
+
+
+
+
+}
+
+
+
+#endif
