5c1c4939bbaad2244dbfeb897fc987efeff803bd
Tobi <tobias.kahlert@student.kit.edu>
2013-05-28 11:21:33 +0200

    Fixed namespace problem

--- library/settings.hpp
+++ library/settings.hpp
@@ -69,8 +69,9 @@ typedef basic_vector2<TNRATIO>		Ratio;
 typedef basic_vector2<float>		Vector;
 
 
+namespace std {
 template<typename _Ty1, typename _Ty2>
-struct std::hash<std::pair<_Ty1, _Ty2> >
+struct hash<std::pair<_Ty1, _Ty2> >
 {
 	std::hash<_Ty1> hash1;
 	std::hash<_Ty2> hash2;
@@ -79,7 +80,7 @@ public:
 		return hash1(p.first) + hash2(p.second);
 	}
 };
-
+}
 
 /// @endcond
 
