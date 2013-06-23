b1ec9e4726ce06a784ac6a571b5dde07af14ec0d
SirTobi <Code.DataByte@gmail.com>
2013-06-23 17:36:44 +0200

    test simplified

--- samples/test/test.cpp
+++ samples/test/test.cpp
@@ -1,75 +1,133 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <memory>
+#include <vector>
 #include "tilenet.h"
 
 
 #define EVENT_BUF_LENGTH 100
 #define COL_WHITE 0xFFFFFFFF
 
+
+class StdTile
+{
+public:
+	StdTile(const std::wstring& name)
+	{
+		tilenet_stdtile(name.c_str(), &id);
+	}
+
+	TNTILE operator ()() const
+	{
+		return (*this)(COL_WHITE);
+	}
+
+	TNTILE operator ()(TNCOLOR color) const
+	{
+		TNTILE tile;
+		tile.type = TN_STD_TILE;
+		tile.data.stdset.id = id;
+		tile.data.stdset.color = color;
+		tile.data.stdset.modifier = 0;
+
+		return tile;
+	}
+
+private:
+	TNID id;
+};
+
+class Layer
+{
+public:
+	Layer(unsigned int w, unsigned int h)
+		: m_width(w), m_height(h)
+		, m_content(w * h), m_updated(w * h, 0)
+	{
+		tilenet_create_tilelayer(&m_handle, w, h, TNSTDRATIO, TNSTDRATIO, 0);
+	}
+
+	const TNTILE& tile(unsigned int x, unsigned int y) const
+	{
+		return tile(y * m_width + x);
+	}
+
+	TNTILE& tile(unsigned int x, unsigned int y)
+	{
+		return tile(y * m_width + x);
+	}
+
+	const TNTILE& tile(unsigned int pos) const
+	{
+		return m_content[pos];
+	}
+
+	TNTILE& tile(unsigned int pos)
+	{
+		m_updated[pos] = 1;
+		return m_content[pos];
+	}
+
+	void flush()
+	{
+		tilenet_update_tilelayer(m_handle, m_content.data(), m_updated.data());
+		memset(m_updated.data(), 0, m_width * m_height * sizeof(TNTILE));
+	}
+
+	void attachTo(TNPARTICIPANT p)
+	{
+		tilenet_attach_layer(p, m_handle);
+	}
+
+	unsigned int width() const { return m_width; }
+	unsigned int height() const { return m_height; }
+
+private:
+	const unsigned int m_width, m_height;
+	TNLAYER m_handle;
+	std::vector<TNTILE> m_content;
+	std::vector<TNBOOL> m_updated;
+};
+
+
+
 #define LAYER_W	10
 #define LAYER_H 10
 
 TNSERVER srv;
-TNID testTileId = 0;
-TNID testSetId;
-TNTILESET testSet;
-TNLAYER testLayer;
-TNACCEPTOR local_acceptor;
+StdTile a_tile(L"test-a");
+Layer* layer;
 TNTILE* layerContent;
 
 
-TNTILE& get_tile(unsigned int x, unsigned int y)
-{
-	return layerContent[y * LAYER_W + x];
-}
-
 void init_testlayer()
 {
-	tilenet_stdtile(L"test-a", &testTileId);
+	layer = new Layer(10, 10);
 
-	tilenet_create_tilelayer(&testLayer, LAYER_W, LAYER_H, TNSTDRATIO, TNSTDRATIO, 0);
-	layerContent = new TNTILE[LAYER_W * LAYER_H];
-	memset(layerContent, 0, LAYER_W * LAYER_H * sizeof(TNTILE));
-
-	TNTILE tile;
-	tile.type = TN_STD_TILE;
-	tile.data.stdset.id = testTileId;
-	tile.data.stdset.color = COL_WHITE;
-	tile.data.stdset.modifier = 0;
-
-	get_tile(0, 0) = tile;
-
-	tile.data.stdset.color = TNMAKE_COLOR(0xff, 0, 0xff, 0);
-	get_tile(1, 0) = tile;
-
-	tile.data.stdset.color = TNMAKE_COLOR(0xff, 0xff, 0, 0);
-	get_tile(9, 9) = tile;
-	tilenet_update_tilelayer(testLayer, layerContent, nullptr);
+	
+	layer->tile(0,0) = a_tile();
+	layer->tile(1,0) = a_tile(TNMAKE_COLOR(0xff, 0, 0xff, 0));
+	layer->tile(9,9) = a_tile(TNMAKE_COLOR(0xff, 0xff, 0, 0));
+	layer->flush();
 }
 
 unsigned int pos = 0;
 
 void update_layer()
 {
-	TNTILE tile;
-	tile.type = TN_STD_TILE;
-	tile.data.stdset.id = testTileId;
-	tile.data.stdset.color = TNMAKE_COLOR(0xff, rand() % 0xff, rand() % 0xff, rand() % 0xff);
-	tile.data.stdset.modifier = 0;
-
-	layerContent[pos++ % (90) ] = tile;
-	tilenet_update_tilelayer(testLayer, layerContent, nullptr);
+	layer->tile(pos++ % (90) + 10) = a_tile(TNMAKE_COLOR(0xff, rand() % 0xff, rand() % 0xff, rand() % 0xff));
+	layer->flush();
 }
 
 void do_event(TNEVENT& e)
 {
-	switch(e.type)
+	/*switch(e.type)
 	{
 	case TNEV_CONNECT:
 		std::cout << "Player connected!\n";
 
-		tilenet_attach_layer(e.participant, testLayer);
+		layer->attachTo(e.participant);
 		break;
 	case TNEV_DISCONNECT:
 		std::cout << "Player disconnected!\n";
@@ -94,7 +152,7 @@ void do_event(TNEVENT& e)
 		}break;
 	default:
 		break;
-	}
+	}*/
 }
 
 
@@ -108,9 +166,9 @@ int main()
 	config.pkgi = L"tn.org.samples.test";
 	config.options = 0;
 
-	init_testlayer();
-
 	tilenet_set_service_thread_count(1);
+	/*init_testlayer();
+
 
 	tilenet_create_server(&srv, &config);
 	tilenet_add_local_acceptor(0, srv);
@@ -135,7 +193,7 @@ int main()
 		}
 
 	}
-
+	*/
 
 
 
