3f6384504aec628286c9edcc6c748bed5834c86d
SirTobi <Code.DataByte@gmail.com>
2013-07-05 15:18:00 +0200

    game implemented

--- samples/connect-four/game.c
+++ samples/connect-four/game.c
@@ -1,6 +1,8 @@
 #include "game.h"
 #include "player.h"
 
+#include <stdarg.h>
+
 #define BOARD_W 7
 #define BOARD_H 6
 
@@ -12,6 +14,11 @@ TNID lcorner_tile = 0;
 TNID rcorner_tile = 0;
 TNID cursor_tile = 0;
 
+TNKEYCODE escape_key = 0;
+TNKEYCODE use_key = 0;
+TNKEYCODE left_key = 0;
+TNKEYCODE right_key = 0;
+
 void init_tiles()
 {
 	if(cursor_tile == 0)
@@ -22,11 +29,17 @@ void init_tiles()
 		lcorner_tile = get_tileid(L"board-left-corner");
 		rcorner_tile = get_tileid(L"board-right-corner");
 		cursor_tile = get_tileid(L"board-cursor");
+
+		tilenet_keycode(L"escape", &escape_key);
+		tilenet_keycode(L"space", &use_key);
+		tilenet_keycode(L"left", &left_key);
+		tilenet_keycode(L"right", &right_key);
+
 	}
 }
 
 
-
+struct _GameView;
 /*
  *	Board:
  *      v
@@ -40,7 +53,11 @@ void init_tiles()
  */
 typedef struct _Game
 {
-	TNID board[BOARD_W][BOARD_H];
+	TNID board[BOARD_W+2][BOARD_H+2];
+	TNID turn;
+
+	struct _GameView* p1;
+	struct _GameView* p2;
 } Game;
 
 typedef struct _GameView
@@ -56,20 +73,176 @@ typedef struct _GameView
 
 
 
+void set_status_line(Layer* layer, TNCOLOR color, const wchar_t* txt, ...)
+{
+#define BUFFER_SIZE 256
+	wchar_t buf[BUFFER_SIZE];
+	int len;
+
+	va_list args;
+	va_start(args, txt);
+
+	len = vswprintf(buf, BUFFER_SIZE, txt, args);
+
+	wcscat(buf, L"                ");
+	print_chars(layer, 0, BOARD_H + 2, color, buf);
+
+	va_end(args);
+
+	flush_layer(layer);
+}
+
 void move_cursor(GameView* gv, int dt)
 {
-	int new_cursor = gv->cursor;
+	int new_cursor = gv->cursor + dt;
 	if(new_cursor < 0 || new_cursor >= BOARD_W)
 		return;
 
-	set_nulltile(gv->board_layer, 1 + new_cursor, 0);
+	set_nulltile(gv->board_layer, 1 + gv->cursor, 0);
 	set_stdtile(gv->board_layer, 1 + new_cursor, 0, cursor_tile, COLOR_BLUE);
 	flush_layer(gv->board_layer);
+
+	gv->cursor = new_cursor;
 }
 
-void handle_game_event(GameView* g, TNEVENT* evt)
+void print_player_turn(GameView* gv, TNID turn)
 {
+	if(turn == gv->pid)
+	{
+		set_status_line(gv->board_layer, COLOR_LGREY, L"Your turn");
+	}else{
+		set_status_line(gv->board_layer, COLOR_LGREY, L"%lss turn", get_player(turn)->name);
+	}
+}
 
+void print_turn(Game* g)
+{
+	print_player_turn(g->p1, g->turn);
+	print_player_turn(g->p2, g->turn);
+}
+
+
+
+void mark_win(Game* g, int x, int y, int dx, int dy, TNID turn)
+{
+	while(g->board[x][y] == turn)
+	{
+		x += dx; y += dy;
+	}
+
+	x -= dx; y -= dy;
+
+	do {
+		set_stdtile(g->p1->board_layer, x, y, token_tile, 0xFFFFFF00);
+		set_stdtile(g->p2->board_layer, x, y, token_tile, 0xFFFFFF00);
+		x -= dx; y -= dy;
+
+	}while(g->board[x][y] == turn);
+
+}
+
+TNBOOL check_dir(Game* g, int x, int y, int dx, int dy, TNID turn)
+{
+	int count = 0;
+	while(g->board[x][y] == turn)
+	{
+		x += dx; y += dy;
+	}
+
+	x -= dx; y -= dy;
+
+	while(g->board[x][y] == turn)
+	{
+		++count;
+		x -= dx; y -= dy;
+	}
+
+	return count >= 4? 1 : 0;
+}
+
+void print_player_win(GameView* gv, TNID turn)
+{
+	if(gv->pid == turn)
+	{
+		set_status_line(gv->board_layer, COLOR_GREEN, L"Your win!");
+	}else{
+		set_status_line(gv->board_layer, COLOR_RED, L"You lose!");
+	}
+}
+
+void print_win(Game* g, TNID turn)
+{
+	print_player_win(g->p1, turn);
+	print_player_win(g->p2, turn);
+}
+
+TNBOOL check_win(Game* g, int x, int y, TNID turn)
+{
+	int dirs[4][2] = {	1, 1,
+						1, 0,
+						0, 1,
+						1, -1};
+	int idx;
+	for(idx = 0; idx < 4; ++idx)
+	{
+		if(check_dir(g, x, y, dirs[idx][0], dirs[idx][1], turn))
+		{
+			mark_win(g, x, y, dirs[idx][0], dirs[idx][1], turn);
+			print_win(g, turn);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void do_turn(GameView* gv)
+{
+	int idx;
+	Game* g = gv->game;
+	TNID* column = g->board[gv->cursor+1];
+	GameView* other = gv->other;
+	TNID turn = g->turn;
+	TNID pid = gv->pid;
+
+	// check if fulls and if it is our turn
+	if(pid != turn || column[1] != 0)
+		return;
+
+	for(idx = 1; idx <= BOARD_H && column[idx] == 0; ++idx);
+
+	--idx;
+	// update
+	column[idx] = pid;
+	set_stdtile(gv->board_layer, gv->cursor + 1, idx, token_tile, COLOR_GREEN);
+	set_stdtile(other->board_layer, gv->cursor + 1, idx, token_tile, COLOR_RED);
+	
+	if(check_win(g, gv->cursor + 1, idx, turn))
+	{
+		g->turn = 0;
+	}else{
+		g->turn = other->pid;
+		print_turn(g);
+	}
+
+}
+
+void handle_game_event(GameView* gv, TNEVENT* evt)
+{
+	if(evt->type == TNEV_KEYDOWN)
+	{
+		TNKEYCODE key = evt->data.keyevent.key;
+
+		if(key == escape_key)
+		{
+
+		}else if (key == left_key || key == right_key)
+		{
+			move_cursor(gv, key == left_key? -1 : 1);
+		}else if(key == use_key)
+		{
+			do_turn(gv);
+		}
+	}
 }
 
 void game_control(PlayerControl c, void* context, TNID p, TNEVENT* evt)
@@ -98,7 +271,7 @@ void game_control(PlayerControl c, void* context, TNID p, TNEVENT* evt)
 GameView* create_game_view(TNID pid, Game* g)
 {
 	int x,y;
-	Layer* layer = create_layer(BOARD_W + 2, BOARD_H + 2);
+	Layer* layer = create_layer(BOARD_W + 10, BOARD_H + 3);
 	GameView* gv = (GameView*)malloc(sizeof(GameView));
 
 	gv->pid = pid;
@@ -137,18 +310,22 @@ void create_game( TNID p1, TNID p2 )
 	int x,y;
 	Game* g = (Game*)malloc(sizeof(Game));
 
+	g->turn = (rand() % 2? p1 : p2);
+
 	// clear
-	for(x = 0; x < BOARD_W; ++x)
-		for(y = 0; y < BOARD_H; ++y)
+	for(x = 0; x < BOARD_W + 2; ++x)
+		for(y = 0; y < BOARD_H + 2; ++y)
 			g->board[x][y] = 0;
 
 	init_tiles();
 
-	gv1 = create_game_view(p1, g);
-	gv2 = create_game_view(p2, g);
+	g->p1 = gv1 = create_game_view(p1, g);
+	g->p2 = gv2 = create_game_view(p2, g);
 
 	gv1->other = gv2;
 	gv2->other = gv1;
+
+	print_turn(g);
 
 	set_player_event_handler(p1, gv1, &game_control);
 	set_player_event_handler(p2, gv2, &game_control);
