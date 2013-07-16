0988077bf778e31e38141ec895b9b7a60e64bc9d
SirTobi <Code.DataByte@gmail.com>
2013-07-16 18:36:36 +0200

    Virtual package source added.

---
+++ library/client/package/loader/virtual_package_source.cpp
@@ -0,0 +1,27 @@
+#include "includes.hpp"
+#include "virtual_package_source.hpp"
+
+#include <boost/filesystem.hpp>
+
+#include "fs_package_source.hpp"
+
+namespace vps {
+
+
+
+
+
+shared_ptr<VirtualPackageSource> LoadVPSFromSF( const fs::path& path )
+{
+	fs::path cpath = fs::canonical(path);
+
+	if(fs::is_directory(cpath))
+	{
+		return std::make_shared<FsPackageSource>(cpath);
+	}
+
+
+
+}
+
+}
---
+++ library/client/package/loader/virtual_package_source.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef _VIRTUAL_PACKAGE_SOURCE_HPP
+#define _VIRTUAL_PACKAGE_SOURCE_HPP
+
+#include <vector>
+#include "settings.hpp"
+
+
+namespace vps {
+
+
+
+class VirtualPackageSource
+	: public std::enable_shared_from_this<VirtualPackageSource>
+{
+public:
+	virtual shared_ptr<VirtualPackageSource> root() = 0;
+
+	virtual shared_ptr<VirtualPackageSource> dir(const string& path) = 0;
+	virtual shared_ptr<VirtualPackageSource> loadVPS(const string& path) = 0;
+	virtual std::vector<wchar_t> loadText(const string& path) = 0;
+	virtual std::vector<byte> loadRaw(const string& path) = 0;
+
+	virtual bool isRoot() = 0;
+	virtual bool isValidPath(const string& path) = 0;
+};
+
+
+
+shared_ptr<VirtualPackageSource> LoadVPSFromSF(const fs::path& path);
+
+
+
+
+
+
+}
+
+
+
+
+
+
+#endif
